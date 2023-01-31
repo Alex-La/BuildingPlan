cebde58fd0ff86a65149d41431f5a7cb1411f444
Brail-Alex <lolondr19@gmail.com>
2023-02-01 02:17:24 +0300

    everything else

--- src/main/java/org/example/service/BrigadeService.java
+++ src/main/java/org/example/service/BrigadeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+//что угодно
 
 @Service
 public class BrigadeService implements IBrigadeService {
